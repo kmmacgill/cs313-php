998fdbad666360792aa190b923a615149930fd85
Korey MacGill <kmmacgill@gmail.com>
2016-01-22 22:32:12 -0700

    updating files from local directories

--- homework/hwResources/hwHeaderInfo.php
+++ homework/hwResources/hwHeaderInfo.php
@@ -1,9 +1,9 @@
 <?php 
 
 echo '<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="resources/style.css">
+	<link rel="stylesheet" type="text/css" href="hwResources/hwStyle.css">
 	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-	<script type="text/javascript" src="resources/script.js"></script>';
+	<script type="text/javascript" src="hwResources/hwScript.js"></script>';
  ?>
--- homework/survey.php
+++ homework/survey.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
+<?php include 'hwResources/hwHeaderInfo.php' ?>
 	<title>Survey assignment</title>
 </head>
 <body>
+
 <div id="header">
 	<h1>Homework assignment 1: Survey</h1>
+</div>
+<div id="navigation">
 	<a href="/./homework.php">Homework Assignments</a>
 </div>
 
