ac7ffa2163c387b5bb9860236e94b560694dc555
Korey MacGill <kmmacgill@gmail.com>
2016-02-11 14:12:11 -0700

    message

--- booksForCooks/dbConnector.php
+++ booksForCooks/dbConnector.php
@@ -2,10 +2,10 @@
 
 function loadDataBase() 
 {
-	$dbHost = "";
-  	$dbPort = "";
-  	$dbUser = "";
-  	$dbPassword = "";
+    $dbHost = "";
+    $dbPort = "";
+    $dbUser = "";
+    $dbPassword = "";
 
     $dbName = "booksforcooks";
 
@@ -16,6 +16,7 @@ function loadDataBase()
         // Not in the openshift environment
         // so use local
         require("setLocalDatabaseCredentials.php");
+        $db = new PDO("mysql:host=localhost;dbname=booksforcooks", $dbUser, $dbPassword);
     }
     else 
     { 
@@ -24,10 +25,9 @@ function loadDataBase()
         $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
         $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
         $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-        $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-    } 
-
-    $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD'); 
+        $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    }
     return $db;
 }
 ?>
--- booksForCooks/setLocalDatabaseCredentials.php
+++ booksForCooks/setLocalDatabaseCredentials.php
@@ -1,16 +1,8 @@
 <?php
-
-$openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
-
-if ($openShiftVar === null || $openShiftVar == "")
-{
-     // Not in the openshift environment
-     $dbHost = "localhost";
-}
-else 
-{
-     // In the openshift environment 
-     $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-}
+// Not in the openshift environment
+$dbHost = "localhost";
+$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+$dbUser = $_POST['userName'];
+$dbPassword = $_POST['password']; 
 
 ?>
