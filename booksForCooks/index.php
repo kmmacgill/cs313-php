9a7ece05df6c6da6275f100bb7a950ec3f8979e1
Korey MacGill <kmmacgill@gmail.com>
2016-02-11 10:43:26 -0700

    changing index.php more

--- booksForCooks/index.php
+++ booksForCooks/index.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['user']) && !isset($_SESSION['pass'])) {
-	$_SESSION['user'] = $_POST['userName'];
-	$_SESSION['pass'] =  $_POST['password'];
+if (!isset($_SESSION['logged'])) {
+	$use = $_POST['userName'];
+	$pass = $_POST['password'];
+	$_SESSION['logged'] = "yes";
 }
 //admin info login stuff
 $host = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
@@ -11,14 +12,10 @@ $port = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $user = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); 
 $password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 
-//users' input
-$use = $_SESSION['userName'];
-$pass = $_SESSION['password'];
-
 echo ("The user name is: " + $use + " And the Password is: " + $pass);
 
 //check for blank login
-//if ($use && $pass) 
+if ($use && $pass) 
 {
 	try { 
 	    $db = new PDO("mysql:host=$host:$port;dbname=booksforcooks", $user, $password);
@@ -34,7 +31,7 @@ echo ("The user name is: " + $use + " And the Password is: " + $pass);
 	join recipe_book rb on rr.recipe_id = rb.recipe_id 
 	join cookbook cb on rb.cookbook_id = cb.cookbook_id 
 	join users u on cb.user_id = u.user_id
-	WHERE u.user_name = '$use'");
+	WHERE u.user_name = '$_SESSION['use']");
 
 	$dataRow  = "";
 	$dataRow1 = "";
